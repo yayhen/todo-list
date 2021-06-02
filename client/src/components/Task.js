84299543bf56986c92f1551a3ef65c1aabfc875c
yayhen <mlpoknwow@gmail.com>
2021-06-02 21:56:33 +0300

    fix: delete task button and done task button

--- client/src/components/Task.js
+++ client/src/components/Task.js
@@ -1,38 +1,57 @@
 import React, { useState } from 'react'
 import {doneTask} from '../logic/doneTask'
 import {deleteTask} from '../logic/deleteTask'
+import { Chip } from './Chip';
 
 export const Task = (props) => {
-  const [taskHeight, setTAskHeight] = useState('100px');
-  let taskDoneStyle = 'none';
-  if(props.value.isDone === true) {
-    taskDoneStyle = 'line-through';
-  } else {
-    taskDoneStyle = 'none'
+  const [taskHeight, setTAskHeight] = useState('120px');
+  let taskNameStyle = 'none'
+  if(props.value.isDone===true) {
+    taskNameStyle = 'line-through';
   }
-
-  const doneTaskButtonHandler = (event) => {
-    doneTask(props.value.id);
-  }
-
+  const [taskNameDecoration, setTaskNameDecoration] = useState(taskNameStyle);
+  const [displayTask, setDisplayTask] = useState('');
+  
+  const deadlineEnabled = (props.value.deadlineEnabled==='checked')
+                          ? 
+                          props.value.deadlineDate.slice(0, 10)+' '+ props.value.deadlineTime 
+                          :
+                          'no deadline';
+  
+  let chips = [];
+  props.value.tags.forEach(element => {
+    chips.push(<Chip value={element}/>)
+  });
+  
   return (
-    <div className="card" style={{height: taskHeight}}>
+    <div className="card" style={{height: taskHeight, display: displayTask}}>
       <div className="card-content">
         <span className="card-title activator grey-text text-darken-4" 
           onClick={()=> setTAskHeight('300px')} 
-          style={{textDecoration: taskDoneStyle}}>
+          style={{textDecoration: taskNameDecoration}}>
             {props.value.taskName}
           <i className="material-icons right" >
             more_vert 
           </i>
         </span>
-        <p>Deadline: {props.value.deadlineDate.slice(0, 10)+' '+ props.value.deadlineTime}</p>
+        <p>Deadline: {deadlineEnabled}</p>
+        {chips}
       </div>
       <div className="card-reveal" style={{overflow: 'auto'}}>
-        <span className="card-title grey-text text-darken-4"  onClick={()=> setTAskHeight('100px')}>{props.value.taskName}<i className="material-icons right">close</i></span>
+        <span className="card-title grey-text text-darken-4"  
+            onClick={()=> setTAskHeight('120px')}
+            style={{textDecoration: taskNameDecoration}}>{props.value.taskName}<i className="material-icons right">close</i></span>
         <p>{props.value.taskDescription}</p>
-        <button className="waves-effect waves-light btn" onClick={() => {doneTask(props.value.id)}}>Done</button>
-        <button className="waves-effect waves-light btn red darken-3" onClick={() => deleteTask(props.value.id)}>Delete</button>
+        <button className="waves-effect waves-light btn"
+            onClick={() => {
+              doneTask(props.value.id)
+              setTaskNameDecoration('line-through')}}>Done</button>
+        <button className="waves-effect waves-light btn red darken-3" 
+            onClick={() => {
+              deleteTask(props.value.id);
+              setDisplayTask('none')}}>
+          Delete
+        </button>
       </div>
     </div>
   )
--- client/src/pages/LocalTasks.js
+++ client/src/pages/LocalTasks.js
@@ -4,7 +4,19 @@ import { Link } from 'react-router-dom'
 
 export const LocalTasks = () => {
   let tasks = [];
-  tasks = JSON.parse(localStorage.getItem('localTasks')).tasks;
+  try {
+    tasks = JSON.parse(localStorage.getItem('localTasks')).tasks;
+  } catch (e) {
+      return (
+        <div>
+          No tasks
+          <Link to='/'>
+            <button className="waves-effect waves-light btn">Back</button>
+          </Link>
+        </div>
+      )
+  }
+  
   let tasksRender = [];
   tasks.forEach((element, index) => {
     tasksRender.push(<Task value={tasks[index]} key={tasks[index].id}/>)
